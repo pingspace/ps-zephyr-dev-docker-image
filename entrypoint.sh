4674b7269561b8945a48466da4870c79409b617f
Manojkumar Subramaniam <manoj@electrolance.com>
2024-10-22 17:04:45 +0000

    run explicit own entrypoint.sh instead devcontainer & mount home form local .container_home to container's home  & install oh my bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+echo "check what is my home: $HOME" > /home/user/entrypoint.log
+# Check if Oh My Bash is installed
+if [ ! -d "$HOME/.oh-my-bash" ]; then
+  echo "Oh My Bash not found. Installing..."
+  bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+fi
+
 SNUM=$(echo $DISPLAY | sed 's/:\([0-9][0-9]*\)/\1/')
 xvfb-run -n $SNUM -s "-screen 0 1024x768x24" -f ~/.Xauthority openbox-session &
 sleep 1
 x11vnc -display $DISPLAY -usepw -forever -quiet &
 
 exec "$@"
+
+tail -f /dev/null
